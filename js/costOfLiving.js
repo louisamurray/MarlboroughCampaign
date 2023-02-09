628d26584e6c723ac0aff7bdc97a9026bed217d9
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-02-09 15:25:18 +1300

    update city info

--- js/costOfLiving.js
+++ js/costOfLiving.js
@@ -8,21 +8,21 @@ const costOfLivingDifferences = {
   Christchurch: 1.316,
   Hamilton: 0.837,
   Dunedin: 0.951,
-  Wellington: 1.652
+  Wellington: 1.652,
   Tauranga: 1.573,
   Napier: 1.076,
-  PalmerstonNorth: 1.015,
+  "Palmerston North": 1.015,
   Nelson: 1.069,
   Rotorua: 1.058,
 };
-  calculate.addEventListener("click", function() {
-const cost = currentCost.value;
-const city = originCity.value;
-if (city in costOfLivingDifferences) {
-  const formattedCost = (cost / costOfLivingDifferences[city]).toLocaleString("en-NZ", { style: "currency", currency: "NZD" });
-  result.innerHTML =
-    "Your cost of living in Marlborough could be " + formattedCost;
-} else {
-  result.innerHTML = "Cost comparison not available for the selected city";
-}
+calculate.addEventListener("click", function() {
+  const cost = currentCost.value;
+  const city = originCity.value;
+  if (city in costOfLivingDifferences) {
+    const formattedCost = (cost / costOfLivingDifferences[city]).toLocaleString("en-NZ", { style: "currency", currency: "NZD" });
+    result.innerHTML =
+      "Your cost of living in Marlborough could be " + formattedCost;
+  } else {
+    result.innerHTML = "Cost comparison not available for the selected city";
+  }
 });
